87a6fa355a2933e7fb85f8db63537eae156e1311
José Robinaldo Ramos Da Silva Junior <robinaldojunior@gmail.com>
2022-05-20 11:10:50 -0300

    Projeto-Corrigido

--- modulo1/projeto-lista-js/exercicios.js
+++ modulo1/projeto-lista-js/exercicios.js
@@ -79,7 +79,7 @@ function calculaIngressosEspetaculo(custo, valorIngresso) {
 // EXERCÍCIO 08
 function checaStringsMesmoTamanho(string1, string2) {
   // implemente sua lógica aqui
-  console.log(string1.length() === string2.length())
+  return string1.length === string2.length
   
 }
 
@@ -92,35 +92,59 @@ function retornaPrimeiroElemento(array) {
 // EXERCÍCIO 10
 function retornaUltimoElemento(array) {
   // implemente sua lógica aqui 
-  return array [1]
+  return array[array.length -1]
 }
 
 // EXERCÍCIO 11
 function trocaPrimeiroEUltimo(array) {
   // implemente sua lógica aqui
-   array = array.splice(array[0], array.length-1)
+  const elemento1 = retornaPrimeiroElemento(array)
+  const elemento2 = retornaUltimoElemento(array)
+
+  array[0] = elemento2
+  array[array.length - 1] = elemento1
+
    return array
 }
 // EXERCÍCIO 12
 function checaIgualdadeDesconsiderandoCase(string1, string2) {
   // implemente sua lógica aqui
-
+  return string1.toLowerCase() === string2.toLowerCase()
 }
 
 // EXERCÍCIO 13
 function checaRenovacaoRG() {
   // implemente sua lógica aqui
+  // const anoAtual = Number(prompt("Digite o ano atual"))
+  // const anoNascimento = Number(prompt("Digite o ano de nascimento"))
+  // const anoEmissao = Number(prompt("Digite o ano de emissão do documento"))
+
+  // const idade = anoAtual - anoNascimento
+  // const tempoCarteira = anoAtual - anoEmissao
+
+  // const cond1 = idade <= 20 && tempoCarteira >= 5
+  // const cond2 = idade > 20 && idade <= 50 && tempoCarteira >= 10
+  // const cond3 = idade > 50 && tempoCarteira >= 15
+
+  // console.log(cond1 || cond2 || cond3)
 
 }
 
 // EXERCÍCIO 14
 function checaAnoBissexto(ano) {
   // implemente sua lógica aqui
+  // const cond1 = ano % 400 === 0
+  // const cond2 = (ano % 4 === 0) && (ano % 100 !== 0)
+  // return cond1 || cond2
 
 }
 
 // EXERCÍCIO 15
 function checaValidadeInscricaoLabenu() {
   // implemente sua lógica aqui
+//   const maiorDeIdade = prompt("Você tem mais de 18 anos?")
+//   const ensinoMedio = prompt("Você possui ensino médio completo?")
+//   const disponibilidade = prompt("Você possui disponibilidade exclusiva durante os horários do curso?")
 
+//   console.log(maiorDeIdade === "sim" && ensinoMedio === "sim" && disponibilidade === "sim")
 }
