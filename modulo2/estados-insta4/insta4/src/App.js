e44c41dba39bbda1541b5dee84f1cb98aba956b5
José Robinaldo Ramos Da Silva Junior <robinaldojunior@gmail.com>
2022-06-09 23:48:43 -0300

    Renderizacao-de-listas

--- modulo2/estados-insta4/insta4/src/App.js
+++ modulo2/estados-insta4/insta4/src/App.js
@@ -1,18 +1,12 @@
 import React from 'react';
-import styled from 'styled-components'
+import React from './App.css';
 import Post from './components/Post/Post';
 
-const MainContainer = styled.div`
-  display: flex;
-  justify-content: center;
-  flex-direction: column;
-  align-items: center;
-`
 
 class App extends React.Component {
   render() {
     return (
-      <MainContainer>
+      <div className={MainContainer}>
         <Post
           nomeUsuario={'paulinha'}
           fotoUsuario={'https://picsum.photos/50/50'}
@@ -28,7 +22,7 @@ class App extends React.Component {
           fotoUsuario={'https://picsum.photos/50/50'}
           fotoPost={'https://picsum.photos/200/150'}
         />
-      </MainContainer>
+      </div>
     );
   }
 }
