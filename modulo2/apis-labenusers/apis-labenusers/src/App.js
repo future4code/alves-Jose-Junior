7a1df979cee9b8696b1fdbc30eba1d1a68b26eeb
José Robinaldo Ramos Da Silva Junior <robinaldojunior@gmail.com>
2022-06-22 19:08:19 -0300

    Aula-revisao-API

--- modulo2/apis-labenusers/apis-labenusers/src/App.js
+++ modulo2/apis-labenusers/apis-labenusers/src/App.js
@@ -1,8 +1,38 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import Cadastro from "./components/Cadastro";
+import ListaUsuarios from "./components/ListaUsario";
 
-function App() {
-  return ()
+
+export default class App extends React.Component {
+  state = {
+    telaAtual: "cadastro"
+  }
+
+  escolheTela = () => {
+    switch (this.state.telaAtual) {
+      case "cadastro":
+        return <Cadastro irParaListaUsuario={this.irParaListaUsuario}/>
+      case "lista": 
+        return <ListaUsuarios irParaCadastro={this.irParaCadastro}/>
+      default:
+        return <div>Erro! Página não encontrada </div>
+    }
+  }
+
+  irParaCadastro = () => {
+    this.setState({telaAtual: "cadastro"})
+  }
+
+  irParaListaUsuario = () => {
+    this.setState({telaAtual: "lista"})
+  }
+
+  render() {
+    return (
+      <div>
+        {this.escolheTela()}
+      </div>
+    )
+  }
 }
 
-export default App;
