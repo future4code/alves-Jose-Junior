db90909366f200270be33b0e0f1dc94dd5485c70
José Robinaldo Ramos Da Silva Junior <robinaldojunior@gmail.com>
2022-07-10 16:54:44 -0300

    instalação-de-material-ui

--- modulo3/projeto-astromatch/astromatch/src/App.js
+++ modulo3/projeto-astromatch/astromatch/src/App.js
@@ -3,13 +3,9 @@ import { useState } from "react";
 import styled from "styled-components";
 import Header from "./components/Header";
 
-const Div = styled.div`
-  display: flex;
-  text-align: center;
-  flex-direction: column;
-  align-items: center;
-  justify-content: center;
-`
+const DivPrincipal = styled.div`
+  
+  `
 export default function App() {
   return (
     <div>
---
+++ modulo3/projeto-astromatch/astromatch/src/Styled.jsx
@@ -0,0 +1,6 @@
+import styled from "styled-components";
+
+export const Titulo = styled.header`
+    align-items: center;
+    background-color: blueviolet;
+`
