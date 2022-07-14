c6d58d37dceef3cadf93f6095c642d578ba949a2
José Robinaldo Ramos Da Silva Junior <robinaldojunior@gmail.com>
2022-07-14 19:01:01 -0300

    rotas

--- modulo3/projeto-labex/projeto-labex/src/App.js
+++ modulo3/projeto-labex/projeto-labex/src/App.js
@@ -1,9 +1,39 @@
 import React from "react";
+import { useNavigate } from "react-router-dom";
+import { CreateTripPage } from "./Pages/CreateTripPage";
+import { TripDetailPage } from "./Pages/TripDetailPage";
+import { BrowserRouter, Routes, Route } from "react-router-dom"
 
 export default function App() {
+  // const navigate = useNavigate()
+
+  // const goToPage1 = () => {
+  //   navigate()
+  // }
+
   return (
     <div>
-      Teste
+      <BrowserRouter>
+        <Routes>
+          <Route index element={<TripDetailPage />} />
+          <Route path="CreateTripPage" element={<CreateTripPage />} />
+
+
+
+        </Routes>
+        
+        
+
+      </BrowserRouter>
+
+
+      <div>
+        <p>Teste</p>
+
+      </div>
+
+      <button>Mudar página</button>
+
     </div>
   );
 }
---
+++ modulo3/projeto-labex/projeto-labex/src/Pages/ListTripsPage.js
@@ -0,0 +1,6 @@
+import React from "react";
+
+export const ListTripsPage = () => {
+    return(
+        <p>ListTripsPage</p>)
+}
